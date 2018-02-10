5155dc3c9d4d5e629823805db217dd7c6a9e772f
CJ <Ershkabible94@gmail.com>
2018-02-09 19:07:31 -0500

    First Commit

---
+++ src/Tools/MyFuncs.java
@@ -0,0 +1,81 @@
+package Tools;
+public class MyFuncs {
+	// Char Class
+	public static class Chars {
+		public static char switchCase(char c) {
+			if (c >= 65 && c <= 90) { // if Uppercase
+				return c += 32;
+			} else if (c >= 97 && c <= 122) { // if Lowercase
+				return c -= 32;
+			}
+
+			return c;
+		}
+
+		public static char toLower(char c) {
+			if (c >= 65 && c <= 90) {
+				return c += 32;
+			}
+			return c;
+		}
+
+		public static char toUpper(char c) {
+			if (c >= 97 && c <= 122) {
+				return c -= 32;
+			}
+			return c;
+		}
+
+		public static void reverseCharArray(char[] c) {
+			char temp;
+
+			for (int i = 0; i < c.length / 2; i++) {
+				temp = c[i];
+				c[i] = c[c.length - 1 - i];
+				c[c.length - 1 - i] = temp;
+			}
+		}
+
+	}
+
+	// Random Class
+	public static class Random {
+		public static int randomIntBetween(int min, int max) {
+			java.util.Random rand = new java.util.Random();
+			return rand.nextInt(max - min + 1) + min;
+		}
+
+		public static double randomDoubleBetween(double min, double max) {
+			java.util.Random rand = new java.util.Random();
+			return min + (max - min) * rand.nextDouble();
+		}
+
+		public static double randomDoubleBetween(double min, double max, int decimalPoints) {
+			java.util.Random rand = new java.util.Random();
+			return Double.parseDouble(String.format("%." + decimalPoints + "f", min + (max - min) * rand.nextDouble()));
+		}
+	}
+
+	public static class Strings {
+		public static String reverseChars(String str) {
+			char[] charArr = new char[str.length()];
+
+			for (int i = 0; i < str.length(); i++) {
+				charArr[str.length() - 1 - i] = str.charAt(i);
+			}
+
+			return String.valueOf(charArr);
+		}
+	}
+
+	public static class Arrays {
+		public static <T> void reverseArray(T[] arr) {
+			T temp;
+			for (int i = 0; i < arr.length / 2; i++) {
+				temp = arr[i];
+				arr[i] = arr[arr.length - 1 - i];
+				arr[arr.length - 1 - i] = temp;
+			}
+		}
+	}
+}
