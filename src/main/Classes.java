3f32c1a760a4c78c2184b7cd173a13ad11722951
CJ <Ershkabible94@gmail.com>
2018-09-25 12:22:33 -0400

    New Clone Updated

--- src/main/Classes.java
+++ src/main/Classes.java
@@ -1,12 +1,11 @@
 package main;
 
+import Tools.BaseConverter;
 import Tools.MyFuncs;
 import Tools.Print;
 
 public class Classes {
 	public static void main(String[] args) {
-		for (int i = 0; i < 100; i++) {
-			Print.pl(MyFuncs.Random.randomDoubleBetween(1, 10, 3));
-		}
+		Print.pl(BaseConverter.convert("f", 18, 5));
 	}
 }
